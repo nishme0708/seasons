01d7aabab3617138181feafff0ededd3f06cf4c5
Nishant Mehta <42231185+nishme0708@users.noreply.github.com>
2021-09-27 00:37:50 +0530

    Add  state

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
-const App = () => {
-    return 'weather';
-};
-
+class App extends React.Component {
+    latitude = 'default';
+    constructor(props) {
+        super(props);
+        this.state = {
+            lat: null,
+            error: ''
+        };
+        window.navigator.geolocation.getCurrentPosition(
+            (pos) => {
+                console.log(pos);
+                this.setState({ lat: pos.coords.latitude });
+            },
+            (err) => {
+                console.log(err);
+                this.setState({ error: err.message });
+            }
+        );
+    }
+    render() {
+        return (
+            <div>
+                Latitude: {this.state.lat}
+                <br />
+                Error: {this.state.error}
+            </div>
+        );
+    }
+}
 ReactDOM.render(<App />, document.querySelector('#root'));
